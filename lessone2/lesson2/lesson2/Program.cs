1455e0e4c58488b0f724c7246ee582b388e390e1
Tovamishkovsky <Hand1e18@mbjcomp.org.il>
2025-09-17 12:04:13 +0300

    Add files via upload

---
+++ lessone2/lesson2/lesson2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<Book>> bookShelves = new List<List<Book>>();
+            List<Book> shelfs1 = new List<Book>();
+            List<Book> shelfs2 = new List<Book>();
+            Book book1 = new Book("shimshon", "yeled", 30, 20, 15);
+            shelfs1.Add(book1);
+            Book book2 = new Book("simcha", "home", 40, 10, 20);
+            shelfs1.Add(book2);
+            bookShelves.Add(shelfs1);
+            Book book3 = new Book("shalom", "yad", 60, 15, 60);
+            shelfs2.Add(book3);
+            Book book4 = new Book("simcha", "home", 55, 7, 10);
+            shelfs2.Add(book4);
+            bookShelves.Add(shelfs2);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ lessone2/lesson2/lesson2/Shelf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    public class Shelf<T> where T : Item 
+    {
+        public int numshelf { get; set; }
+        public int column { get; set; }
+        public double mishkal { get; set; }
+        public int capacity { get; set; }
+
+        public Shelf(int capacity,int numshelf, int column, double mishkal)
+        {
+            this.numshelf = numshelf;
+            this.column = column;
+            this.mishkal = mishkal;
+            this.capacity = capacity;
+        }
+
+    }
+}
